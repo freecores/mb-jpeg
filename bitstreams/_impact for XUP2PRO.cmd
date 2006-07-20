96a9ef0d0d9f6bd8c813cc75c0f0ad04efae2ca2
quickwayne <quickwayne@2fbf6638-bb94-400d-997d-fe0b89308f78>
2006-07-20 22:52:44 +0000

    no message

--- bitstreams/_impact for XUP2PRO.cmd
+++
@@ -1,7 +0,0 @@
-setMode -bs
-setCable -port auto
-identify
-identifyMPM
-assignFile -p 3 -file "implementation/download.bit"
-program -p 3 
-quit
---
+++ bitstreams/download_XUP2PRO.cmd
@@ -0,0 +1,6 @@
+setMode -bscan
+setCable -p auto
+identify
+assignfile -p 3 -file download_XUP2PRO_v0.1.bit
+program -p 3
+quit
