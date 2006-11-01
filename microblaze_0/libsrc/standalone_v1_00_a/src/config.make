a5b6642592ec7c88475251da4fb38982c2e607fd
quickwayne <quickwayne@2fbf6638-bb94-400d-997d-fe0b89308f78>
2006-11-01 12:22:44 +0000

    *** empty log message ***

--- microblaze_0/libsrc/standalone_v1_00_a/src/config.make
+++
@@ -1,3 +0,0 @@
-LIBSOURCES = *.s *.c *.S
-PROFILE_ARCH_OBJS = profile_mcount_mb.o
-LIBS = standalone_libs
--- microblaze_0/libsrc/standalone_v1_00_a/src/inbyte.c
+++
@@ -1,6 +0,0 @@
-#include "xparameters.h"
-#include "xuartlite_l.h"
-
-char inbyte(void) {
-	 return XUartLite_RecvByte(STDIN_BASEADDRESS);
-}
